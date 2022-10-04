fc0204a72c8e34f170b05f7479debeb857344d29
cheesecup <couplejwsa@gmail.com>
2022-10-05 00:55:17 +0900

    #10 - 테스트 데이터를 data.sql에 추가
    * 가상의 테스트 데이터 추가
    * mockaroo 사이트를 통하면 대량의 데이터 생산 가능
    * mockaroo 링크(https://www.mockaroo.com/)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO article(title, content, created_by, modified_by, created_at, modified_at)
+VALUES ('first title', 'first content by cheesecup', 'cheesecup', 'cheesecup', '2021-05-30 23:53:46', '2021-03-10 08:48:50');
+
+INSERT INTO article(title, content, created_by, modified_by, created_at, modified_at)
+VALUES ('second title', 'second content by chiz', 'chiz', 'chiz', '2022-10-04 00:32:25', '2022-10-05 00:33:01');
