72800d62d13838042b7cd319061665692f9d7904
cheesecup <couplejwsa@gmail.com>
2022-10-05 13:18:21 +0900

    #12 - h2-console 옵션 삭제
    h2 는 테스트 전용, 서비스 단계에서 사용 하지 않기 때문에 콘솔이 필요 없다.

--- src/main/java/com/springboot/projectboard/ProjectBoardApplication.java
+++ src/main/java/com/springboot/projectboard/ProjectBoardApplication.java
@@ -2,6 +2,7 @@ package com.springboot.projectboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ProjectBoardApplication {
