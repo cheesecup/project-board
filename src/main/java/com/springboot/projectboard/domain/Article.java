958b9885de76f1f6db7b9bf7da3bcc983e64d9db
cheesecup <couplejwsa@gmail.com>
2022-10-05 13:30:09 +0900

    #12 - AuditingFields 추출
    생성자, 생성일시, 수정자, 수정일시는 반복적으로 엔티티 클래스에 들어가는 요소이고, 도메인과 직접적인 연관이 없으므로 `@MappedSuperclass` 를 이용해서 상속 방식으로 추출
    그 외 `@DateTimeFormat` 추가 및 `@Column` 옵션 개선

--- src/main/java/com/springboot/projectboard/domain/Article.java
+++ src/main/java/com/springboot/projectboard/domain/Article.java
@@ -3,14 +3,8 @@ package com.springboot.projectboard.domain;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -24,8 +18,7 @@ import java.util.Set;
         @Index(columnList = "createdAt"),
         @Index(columnList = "createdBy"),
 })
-@EntityListeners(AuditingEntityListener.class)
-public class Article {
+public class Article extends AuditingFields {
 
     /* MySQl Auto Increment = IDENTITY 타입*/
     @Id
@@ -47,22 +40,6 @@ public class Article {
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     @ToString.Exclude
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime createdAt; // 생성일시
-
-    @CreatedBy
-    @Column(nullable = false, length = 100)
-    private String createdBy; // 생성자
-
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt; // 수정 일시
-
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy; // 수정자
 
     protected Article() {}
 
