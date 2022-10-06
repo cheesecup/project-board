41945e48e6b53cdd3591f4927b4762397c32f44e
cheesecup <couplejwsa@gmail.com>
2022-10-07 02:53:20 +0900

    #16 - 컨트롤러 테스트 업데이트
    미디어 타입 검사 방법을 4ab1d65 에서 확인했기 때문에, 나머지 뷰도 동일하게 작동할 것으로 예상하고
    미리 테스트 메소드를 업데이트

--- src/test/java/com/springboot/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/springboot/projectboard/controller/ArticleControllerTest.java
@@ -37,7 +37,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestingArticleView_thenReturnsArticleView() throws Exception{
         mvc.perform(get("/articles/1"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/detail"))
                 .andExpect(model().attributeExists("article"))
                 .andExpect(model().attributeExists("articleComments"));
@@ -50,7 +50,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestingArticleSearchView_thenReturnsArticleSearchView() throws Exception{
         mvc.perform(get("/articles/search"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search"));
     }
 
@@ -60,7 +60,7 @@ class ArticleControllerTest {
     void givenNothing_whenRequestingArticleHashtagSearchView_thenReturnsArticleHashtagSearchView() throws Exception{
         mvc.perform(get("/articles/search-hashtag"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("articles/search-hashtag"));
     }
 
